cd7b45cb15602e515a41fda8df3a5e5ea5671706
= <pavannayak54348@gmail.com>
2024-06-12 08:32:35 +0530

    Updated Suspect Cotroller to provide image upload.

--- Controllers/SuspectsController.cs
+++ Controllers/SuspectsController.cs
@@ -7,18 +7,24 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using CrimeAdminAPI.Database;
 using CrimeAdminAPI.Models;
+using Azure.Storage.Blobs;
 
 namespace CrimeAdminAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class SuspectsController : ControllerBase
-    {
+    {   
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly string _containerNameForImage = "suspectimages"; // Replace with your container name
+
         private readonly CrimeDbContext _context;
 
-        public SuspectsController(CrimeDbContext context)
+        public SuspectsController(CrimeDbContext context, BlobServiceClient blobServiceClient)
         {
             _context = context;
+            _blobServiceClient = blobServiceClient;
+
         }
 
         // GET: api/Suspects
@@ -84,12 +90,20 @@ namespace CrimeAdminAPI.Controllers
         // POST: api/Suspects
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<Suspect>> PostSuspect(Suspect suspect)
+        public async Task<ActionResult<Suspect>> PostSuspect([FromForm] Suspect suspect, IFormFile imageFile)
         {
           if (_context.Suspect == null)
           {
               return Problem("Entity set 'CrimeDbContext.Suspect'  is null.");
           }
+
+          if (imageFile != null && imageFile.Length > 0)
+          {
+            string imageUrl = await UploadFileToBlobStorageImage(imageFile);
+                suspect.SuspectImageUrl = imageUrl;
+          }
+
+
             _context.Suspect.Add(suspect);
             await _context.SaveChangesAsync();
 
@@ -119,6 +133,16 @@ namespace CrimeAdminAPI.Controllers
         private bool SuspectExists(int id)
         {
             return (_context.Suspect?.Any(e => e.SuspectID == id)).GetValueOrDefault();
+        }
+        private async Task<string> UploadFileToBlobStorageImage(IFormFile file)
+        {
+            var blobContainerClient = _blobServiceClient.GetBlobContainerClient(_containerNameForImage);
+            var blobClient = blobContainerClient.GetBlobClient(Guid.NewGuid().ToString() + Path.GetExtension(file.FileName));
+            using (var stream = file.OpenReadStream())
+            {
+                await blobClient.UploadAsync(stream, true);
+            }
+            return blobClient.Uri.ToString();
         }
     }
 }
